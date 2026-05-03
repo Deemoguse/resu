d9c1f6548c9335f11884e0f09c2cb4ee748a3975
Ruslan Radionov <r.radionov@riverstart.ru>
2026-05-03 20:26:45 +0300

    feature/inferization-type-eslint: Increasing the rigor of type checking.

--- packages/internal.configs/base/eslint.config.js
+++ packages/internal.configs/base/eslint.config.js
@@ -16,18 +16,24 @@ export function createConfig (...args) {
 	return defineConfig([
 		{
 			ignores: [
-				'dist/**/*'
+				'dist/**/*',
+				'eslint.config.ts',
 			]
 		},
 
-		eslintJS.configs.recommended,
-		eslintTS.configs.strict,
 		eslintStylistic.configs.recommended,
-
+		eslintJS.configs.recommended,
+		eslintTS.configs.strictTypeChecked,
 		{
+			languageOptions: {
+				parserOptions: {
+					projectService: true,
+				},
+			},
 			rules: {
 				'@typescript-eslint/no-namespace': 'off',
 				'@typescript-eslint/no-explicit-any': 'off',
+				'@typescript-eslint/no-confusing-void-expression': 'off',
 			}
 		},
 
